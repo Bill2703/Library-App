9fb5a56dd0f48b665d772cd22725703af501fdae
Khider <Khider.Tariq1@gmail.com>
2023-11-26 16:29:30 +0000

    checks if book already exists before creating

--- server/models/Book.js
+++ server/models/Book.js
@@ -21,19 +21,35 @@ class Book{
     static async getOneByBookName(bookName){
         const response = await db.query("SELECT * FROM book WHERE LOWER(title) = $1",[bookName])
         //console.log(response.rows);
-        if(response.rows.length != 1) {
+        if (response.rows.length > 1 ){
+            throw new Error("More than one book of same name!")
+        }
+        else if(response.rows.length != 1) {
             throw new Error("Unable to find book!")
         }
         return new Book(response.rows[0])
     }
 
-    static async create(data){
-        const {title, author, blurb, stock} = data;
-        const response = await db.query("INSERT INTO book (title, author, blurb, stock) VALUES ($1, $2, $3, $4) RETURNING title;", [title, author, blurb, stock])
+    static async create(data) {
+        const { title, author, blurb, stock } = data;
+    
+        // Check if a book with the same title already exists
+        const existingBook = await db.query("SELECT * FROM book WHERE title = $1", [title]);
+        if (existingBook.rows.length > 0) {
+            throw new Error(`A book with the title '${title}' already exists.`);
+        }
+    
+        // If not, proceed with the insertion
+        const response = await db.query(
+            "INSERT INTO book (title, author, blurb, stock) VALUES ($1, $2, $3, $4) RETURNING title;",
+            [title, author, blurb, stock]
+        );
+    
         const bookName = response.rows[0].title.toLowerCase();
         const newBook = await Book.getOneByBookName(bookName);
         return newBook;
     }
+    
 
 }
 

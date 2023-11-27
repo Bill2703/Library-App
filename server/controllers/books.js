a03695a172e8326cb232e951ca3e48ca109a6a27
Khider <Khider.Tariq1@gmail.com>
2023-11-27 11:20:32 +0000

    adds images to book table

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -3,6 +3,7 @@ const Book = require("../models/Book")
 async function index(req,res) {
     try {
         const books = await Book.getAll();
+        //console.log(books);
         res.status(200).json(books)
     } catch(err) {
         res.status(500).json({error: err.message})
--- server/models/Book.js
+++ server/models/Book.js
@@ -1,16 +1,19 @@
 const db = require("../database/connect")
 
 class Book{
-    constructor({book_id, title, author, blurb, stock}) {
+    constructor({book_id, title, author, blurb, stock, coverimageurl}) {
         this.id = book_id
         this.title = title
         this.author = author
         this.blurb = blurb
         this.stock = stock
+        this.coverimageurl = coverimageurl
     }
+    
 
     static async getAll(){
         const response = await db.query("SELECT * FROM book");
+        console.log(response.rows);
         if(response.rows.length === 0){
             throw new Error("No books available!")
         } else {
@@ -31,7 +34,7 @@ class Book{
     }
 
     static async create(data) {
-        const { title, author, blurb, stock } = data;
+        const { title, author, blurb, stock, coverimageurl } = data;
     
         const existingBook = await db.query("SELECT * FROM book WHERE title = $1", [title]);
         if (existingBook.rows.length > 0) {
@@ -39,8 +42,8 @@ class Book{
         }
     
         const response = await db.query(
-            "INSERT INTO book (title, author, blurb, stock) VALUES ($1, $2, $3, $4) RETURNING title;",
-            [title, author, blurb, stock]
+            "INSERT INTO book (title, author, blurb, stock, coverimageurl) VALUES ($1, $2, $3, $4, $5s) RETURNING title;",
+            [title, author, blurb, stock, coverimageurl]
         );
     
         const bookName = response.rows[0].title.toLowerCase();
@@ -55,7 +58,7 @@ class Book{
     
 
     async update(data) {
-        const { title, author, blurb, stock } = data;
+        const { title, author, blurb, stock, coverimageurl } = data;
     
         if (title !== this.title) {
             const existingBook = await db.query("SELECT * FROM book WHERE title = $1", [title]);
@@ -65,8 +68,8 @@ class Book{
         }
 
         const response = await db.query(
-            "UPDATE book SET title=$1, author=$2, blurb=$3, stock=$4 WHERE title=$5 RETURNING *",
-            [title, author, blurb, stock, this.title]
+            "UPDATE book SET title=$1, author=$2, blurb=$3, stock=$4, coverimageurl=$5 WHERE title=$6 RETURNING *",
+            [title, author, blurb, stock, coverimageurl, this.title]
         );
     
         const bookName = response.rows[0].title.toLowerCase();

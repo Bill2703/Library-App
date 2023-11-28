1eae3269ccd57a0bc8ac8c81e2a15cd92b4f53f0
Khider <Khider.Tariq1@gmail.com>
2023-11-28 10:25:19 +0000

    creates rental table, connects it to elephantsql, fixes index.js

--- server/database/setup.js
+++ server/database/setup.js
@@ -5,12 +5,14 @@ const db = require("./connect");
 
 const books = fs.readFileSync('./database/books.sql').toString();
 const users = fs.readFileSync("./database/users.sql").toString();
+const book_rental = fs.readFileSync("./database/booking.sql").toString();
 
 
 const setUp = async () => {
     try{
         await db.query(books);
         await db.query(users);
+        await db.query(book_rental);
         console.log("DB Setup and Seeded 🌱.");
         db.end();
     } catch (error) {
--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
-
+const api = require("./api")
 const PORT = process.env.PORT || 3000; // Fallback to 3000 if process.env.PORT is undefined
 
-app.listen(PORT, () => {
+api.listen(PORT, () => {
     console.log(`API listening on ${PORT} 👂`);
 });

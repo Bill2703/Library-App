1aa12b669ff0a99bf023e6cace9e124e2291a883
Khider <Khider.Tariq1@gmail.com>
2023-11-28 10:04:22 +0000

    sends user back to book page, removes chosen book from local storage, adds authenticator to rental page

--- client/assets/bookingSystem.js
+++ client/assets/bookingSystem.js
@@ -2,7 +2,6 @@ const selectedBook = JSON.parse(localStorage.getItem('selectedBook'));
 console.log(selectedBook);
 
 bookName = selectedBook.title;
-
 const nameSpace = document.getElementById("additionalInfo");
 nameSpace.textContent=bookName;
 
@@ -11,9 +10,14 @@ async function updateStock(selectedBook) {
         // Extract relevant data (e.g., book name)
         const { title, stock } = selectedBook;
 
-        console.log(selectedBook);
+        const options = {
+            headers: {
+                authorization: localStorage.getItem("token"),
+            }
+        };
+
         // Craft a PATCH request to update the stock
-        const response = await fetch(`http://localhost:3000/books/stock/${selectedBook.title}`, {
+        const response = await fetch(`http://localhost:3000/books/stock/${title}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
@@ -22,27 +26,31 @@ async function updateStock(selectedBook) {
             body: JSON.stringify({
                 // Include the data you want to update, for example, reducing the stock by 1
                 stock: stock - 1,
-                title: selectedBook.title
+                title: title,
             }),
         });
-
-        //console.log(response);
 
         if (!response.ok) {
             throw new Error('Failed to update stock');
         }
 
         // Successfully updated stock, now you can redirect to the booking page or perform other actions
-        // ...
+        // For example, redirecting to a booking page
+        window.location.assign("./bookpage.html");
 
         // Clear the selectedBook from localStorage (optional)
         localStorage.removeItem('selectedBook');
     } catch (error) {
+        // Handle errors, for example, show an error message to the user
         console.error('Error updating stock:', error);
+        // Show an error message to the user (you might want to implement this part)
     }
 }
 
-const bookNowLink = document.getElementById("bookNow")
-bookNowLink.addEventListener('click', function () {
+const bookNowLink = document.getElementById("bookNow");
+
+bookNowLink.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent the default behavior of the link
     updateStock(selectedBook);
 });
+

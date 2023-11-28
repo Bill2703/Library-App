378ca7fe8879612e9f1a2a0ddfbc77561c8d52aa
Raeannox1 <nyagreen20@gmail.com>
2023-11-28 11:27:06 +0000

    if statement for time and date

--- client/assets/bookingSystem.js
+++ client/assets/bookingSystem.js
@@ -1,31 +1,26 @@
+// Retrieve and display the selected book's details
 const selectedBook = JSON.parse(localStorage.getItem('selectedBook'));
-console.log(selectedBook);
-
-bookName = selectedBook.title;
 const nameSpace = document.getElementById("additionalInfo");
-nameSpace.textContent=bookName;
+nameSpace.textContent = selectedBook.title;
 
-async function updateStock(selectedBook) {
+// Get input fields for booking date and time
+const bookingDateInput = document.getElementById('date');
+const bookingTimeInput = document.getElementById('time');
+
+// Function to update the stock of the selected book
+async function updateStock(book) {
     try {
-        // Extract relevant data (e.g., book name)
-        const { title, stock } = selectedBook;
+        const { title, stock } = book;
 
-        const options = {
-            headers: {
-                authorization: localStorage.getItem("token"),
-            }
-        };
-
-        // Craft a PATCH request to update the stock
+        // Sending a PATCH request to update the stock of the book
         const response = await fetch(`http://localhost:3000/books/stock/${title}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem('token'), // Include authorization header if needed
+                'Authorization': localStorage.getItem('token'), // Authorization header for secured endpoints
             },
             body: JSON.stringify({
-                // Include the data you want to update, for example, reducing the stock by 1
-                stock: stock - 1,
+                stock: stock - 1, // Decrease stock by 1
                 title: title,
             }),
         });
@@ -34,34 +29,40 @@ async function updateStock(selectedBook) {
             throw new Error('Failed to update stock');
         }
 
-        // Successfully updated stock, now you can redirect to the booking page or perform other actions
-        // For example, redirecting to a booking page
+        // On successful stock update, redirect to the booking page
         window.location.assign("./bookpage.html");
-
-        // Clear the selectedBook from localStorage (optional)
-        localStorage.removeItem('selectedBook');
     } catch (error) {
-        // Handle errors, for example, show an error message to the user
+        // Log and show error message to the user
         console.error('Error updating stock:', error);
-        // Show an error message to the user (you might want to implement this part)
     }
 }
 
+// Event listener for the 'Book Now' button
 const bookNowLink = document.getElementById("bookNow");
-
 bookNowLink.addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent the default behavior of the link
+    event.preventDefault();
+
+    // Validate date and time inputs before proceeding
+    const bookingDate = bookingDateInput.value;
+    const bookingTime = bookingTimeInput.value;
+    if (!bookingDate || !bookingTime) {
+        alert("Please enter both date and time for the booking.");
+        return;
+    }
+
+    // Proceed to update the book's stock
     updateStock(selectedBook);
 });
 
-
+// Event listener for the 'Logout' button
 const logout = document.getElementById("logout");
 logout.addEventListener("click", () => {
-    localStorage.removeItem("token")
-    localStorage.removeItem("selectedBook")
+    localStorage.removeItem("token");
+    localStorage.removeItem("selectedBook");
 });
 
+// Event listener for the 'Back' button
 const back = document.getElementById("back");
 back.addEventListener("click", () => {
-    localStorage.removeItem("selectedBook")
-})
+    localStorage.removeItem("selectedBook");
+});

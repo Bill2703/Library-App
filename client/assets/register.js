dc3ab081f833ab0d80947646333e68dac81d0887
laplata247 <enquiriesmbp@gmail.com>
2023-11-30 12:24:54 +0000

    making final css channges

--- client/assets/register.js
+++ client/assets/register.js
@@ -189,23 +189,22 @@ document
   const eyeIcon = document.querySelector(".pass-field i");
   const requirementList = document.querySelectorAll(".requirement-list li");
 
-  // An array of password requirements with corresponding
-  // regular expressions and index of the requirement list item
+
   const requirements = [
-      { regex: /.{8,}/, index: 0 }, // Minimum of 8 characters
-      { regex: /[0-9]/, index: 1 }, // At least one number
-      { regex: /[a-z]/, index: 2 }, // At least one lowercase letter
-      { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
-      { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
+      { regex: /.{8,}/, index: 0 }, 
+      { regex: /[0-9]/, index: 1 }, 
+      { regex: /[a-z]/, index: 2 }, 
+      { regex: /[^A-Za-z0-9]/, index: 3 },
+      { regex: /[A-Z]/, index: 4 }, 
   ]
 
   passwordInput.addEventListener("keyup", (e) => {
       requirements.forEach(item => {
-          // Check if the password matches the requirement regex
+          
           const isValid = item.regex.test(e.target.value);
           const requirementItem = requirementList[item.index];
 
-          // Updating class and icon of requirement item if requirement matched or not
+        
           if (isValid) {
               requirementItem.classList.add("valid");
               requirementItem.firstElementChild.className = "fa-solid fa-check";
